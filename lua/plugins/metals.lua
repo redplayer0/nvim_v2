aaff5fef8809f4773446b06f05074604e925cc94
redplayer0 <mr_red83@protonmail.com>
2025-08-06 12:15:22 +0300

    update

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,34 +1,77 @@
 return {
   "scalameta/nvim-metals",
-  name = "metals",
-  ft = { "scala", "sbt", "java" },
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    -- {
+    --   "j-hui/fidget.nvim",
+    --   opts = {},
+    -- },
+    {
+      "mfussenegger/nvim-dap",
+      config = function(self, opts)
+        -- Debug settings if you're using nvim-dap
+        local dap = require("dap")
+
+        dap.configurations.scala = {
+          {
+            type = "scala",
+            request = "launch",
+            name = "RunOrTest",
+            metals = {
+              runType = "runOrTestFile",
+              --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+            },
+          },
+          {
+            type = "scala",
+            request = "launch",
+            name = "Test Target",
+            metals = {
+              runType = "testTarget",
+            },
+          },
+        }
+      end
+    },
   },
-  -- stylua: ignore
-  keys = {
-    { "<leader>cW", function() require('metals').hover_worksheet() end,               desc = "Metals Worksheet" },
-    { "<leader>cM", function() require('telescope').extensions.metals.commands() end, desc = "Telescope Metals Commands" },
-  },
-  config = function()
+  ft = { "scala", "sbt", "java" },
+  opts = function()
     local metals_config = require("metals").bare_config()
 
+    -- Example of settings
     metals_config.settings = {
       showImplicitArguments = true,
-      showImplicitConversionsAndClasses = true,
-      showInferredType = true,
-      superMethodLensesEnabled = true,
+      excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
     }
-    metals_config.init_options.statusBarProvider = "on"
-    metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+    -- *READ THIS*
+    -- I *highly* recommend setting statusBarProvider to either "off" or "on"
+    --
+    -- "off" will enable LSP progress notifications by Metals and you'll need
+    -- to ensure you have a plugin like fidget.nvim installed to handle them.
+    --
+    -- "on" will enable the custom Metals status extension and you *have* to have
+    -- a have settings to capture this in your statusline or else you'll not see
+    -- any messages from metals. There is more info in the help docs about this
+    -- metals_config.init_options.statusBarProvider = "off"
+
+    -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+    -- metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    metals_config.on_attach = function(client, bufnr)
+      require("metals").setup_dap()
+    end
+
+    return metals_config
+  end,
+  config = function(self, metals_config)
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "scala", "sbt", "java" },
+      pattern = self.ft,
       callback = function()
         require("metals").initialize_or_attach(metals_config)
       end,
       group = nvim_metals_group,
     })
-  end,
+  end
+
 }
